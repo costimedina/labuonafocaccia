6d9b87c92e338e0715bd6a003b6305d02c4fc931
Francisca-Olaeta <francisca.olaeta@gmail.com>
2023-01-11 11:59:17 -0300

    variables and theme colors set

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //STYLES
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.scss';
 import { Button } from "react-bootstrap";
 
 
@@ -8,7 +8,14 @@ function App() {
   return (
     <div className="App">
      <h1 className="title">hola, como estás?</h1>
-     <Button className="btn btn-primary">enviar</Button>
+     <Button className="btn btn-primary">Primary</Button>
+     <Button className="btn btn-secondary">Secondary</Button>
+     <Button className="btn btn-terciary">Terciary</Button>
+     <Button className="btn btn-danger">Danger</Button>
+     <Button className="btn btn-success">Success</Button>
+     <Button className="btn btn-warning">Warning</Button>
+     <Button className="btn btn-dark">Dark</Button>
+     <Button className="btn btn-light">Light</Button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import "bootstrap/dist/css/bootstrap.min.css";
+
 import './sass/main.scss'
-import './App.scss';
+
 
 
 
