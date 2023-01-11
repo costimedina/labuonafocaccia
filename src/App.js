2878e33c38c7086c23ab91e45f77135616b65590
costimedina <costy.medina@gmail.com>
2023-01-10 22:49:06 -0400

    sass properties

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 //STYLES
-import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import './App.scss';
+import { Button } from "react-bootstrap";
+
 
 function App() {
   return (
     <div className="App">
-     
+     <h1 className="title">hola, como estás?</h1>
+     <Button className="btn btn-primary">enviar</Button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './sass/main.scss'
+import './App.scss';
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
