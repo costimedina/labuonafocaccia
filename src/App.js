55cef464f1acebb8e88361861977fbf2e9bad704
costimedina <costy.medina@gmail.com>
2023-01-17 20:06:54 -0400

    new route at app.js in develop branch

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
 //STYLES
-import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button } from "react-bootstrap";
 
+//COMPONENTS
+import NavbarPrimario from "./components/NavbarPrimario";
+
+//HOOKS
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//VIEWS 
+import Landing from "./views/Landing";
 
 function App() {
   return (
-    <div className="App">
-     <h1>LA BUONA FOCACCIA H1</h1>
-     <h2>LA BUONA FOCACCIA H2</h2>
-     <h3>LA BUONA FOCACCIA h3</h3>
-     <h4>LA BUONA FOCACCIA h4</h4>
-     <h5>LA BUONA FOCACCIA h5</h5>
-  
 
-     <Button className="btn btn-primary boton">Primary</Button>
-     <Button className="btn btn-secondary">Secondary</Button>
-    
-     <Button className="btn btn-dark">Dark</Button>
-     <Button className="btn btn-light">Light</Button>
-
-
-
-     <p>Prueba de tipografía</p>
-    </div>
+<BrowserRouter>
+<NavbarPrimario />
+  <Routes>
+    <Route path="/" element={<Landing />} />
+  </Routes>
+</BrowserRouter>
   );
 }
 
