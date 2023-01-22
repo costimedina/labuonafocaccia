24af68dd22b6115daeb22f03d1ef9c4c924642d7
costimedina <costy.medina@gmail.com>
2023-01-22 11:54:33 -0400

    Co-authored-by: JesusTruyol <JesusTruyol@users.noreply.github.com> Co-authored-by: Francisca-Olaeta <Francisca-Olaeta@users.noreply.github.com>

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+<>
+<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, nobis? Commodi nam qui debitis nemo exercitationem, dignissimos reprehenderit fugit dolorum labore, quia delectus mollitia voluptates, tempora optio amet veritatis esse?</div>
+</>
+    );
+};
+
+export default Footer;
