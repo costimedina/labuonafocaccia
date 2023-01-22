ec5fee4bb34fc0d1936ea6415e399ebdcf6e1ca9
costimedina <costy.medina@gmail.com>
2023-01-22 12:27:24 -0400

    footer on development

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,31 @@
-function Footer(){
-    return(
-<>
-<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, nobis? Commodi nam qui debitis nemo exercitationem, dignissimos reprehenderit fugit dolorum labore, quia delectus mollitia voluptates, tempora optio amet veritatis esse?</div>
-</>
+function Footer() {
+    return (
+        <>
+            <div className="main-footer">
+                <div className="container">
+                    <div className="row">
+                        {/* Column 1 */}
+                        <div className="col-md-3 col-sm-6">
+                            <h4>Lorem ipsum</h4>
+                            <p>Lorem ipsum</p>
+                            <p>Lorem ipsum</p>
+                        </div>
+                        {/* Column 2 */}
+                        <div className="col-md-3 col-sm-6">
+                            <h4>Lorem ipsum</h4>
+                            <p>Lorem ipsum</p>
+                            <p>Lorem ipsum</p>
+                        </div>
+                        {/* Column 3 */}
+                        <div className="col-md-3 col-sm-6">
+                            <h4>Lorem ipsum</h4>
+                            <p>Lorem ipsum</p>
+                            <p>Lorem ipsum</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
