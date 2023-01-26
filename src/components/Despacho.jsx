20ecbdc7cf6048489bd491551d6068cbcec7ef2b
costimedina <costy.medina@gmail.com>
2023-01-25 22:32:15 -0400

    map is added

--- src/components/Despacho.jsx
+++ src/components/Despacho.jsx
@@ -1,12 +1,17 @@
-import { Map } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
+import "../assets/styles/despacho.css";
 
 function Despacho() {
+  const position = [-33.41588113551994, -70.59482124089861];
   return (
-    <Map center={{lat: '51.52437', lng: '13.41053'}} zoom={13}>
-
-    </Map>
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
   );
 };
 
-export default Despacho
+export default Despacho;
