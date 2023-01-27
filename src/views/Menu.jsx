a587d45bad5037396f4742998f36cabd5ad3e181
Francisca-Olaeta <francisca.olaeta@gmail.com>
2023-01-27 14:04:52 -0300

    added menu sections

--- src/views/Menu.jsx
+++ src/views/Menu.jsx
@@ -6,8 +6,21 @@ import Grid from '../components/Grid';
 const Menu = () => {
   return (
     <Container className='mainCont p-0'>
+      <section>
         <h2 className="mainCont__title">DE LA BUONA</h2>
         <Grid />
+      </section>
+
+      <section className='mt-5'>
+        <h2 className="mainCont__title">DE LA BUONA</h2>
+        <Grid />
+      </section>
+
+      <section className='mt-5'>
+        <h2 className="mainCont__title">DE LA BUONA</h2>
+        <Grid />
+      </section>
+    
         
          
 
